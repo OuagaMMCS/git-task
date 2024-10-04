fd8ef3ffd3bea72b8b6a6a0b5304dd26935996d2
Maksim Levko <chepapguevara@gmail.com>
2024-10-04 20:06:39 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var Year := ReadInteger('Введите год:');
+  Assert(Year > 0);
+  var IsV := False;
+  if ((Year mod 4 = 0) and (Year mod 100 <> 0)) or (Year mod 400 = 0) then
+    IsV := True;
+  Print($'Год високосный: {IsV}');
+end.
